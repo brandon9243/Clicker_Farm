80059a7b660f8f295517558372fba4c409cc1386
brandon9243 <100967878+brandon9243@users.noreply.github.com>
2024-01-30 21:56:56 -0500

    Delete scripts directory
    Replaced with src

--- scripts/building.js
+++
@@ -1,9 +0,0 @@
-class Building {
-    constructor() {}
-
-    // Triggered when the player buys a Building
-    purchase() {}
-
-    // Updates the button, grey if can't afford, price, number owned, etc
-    buttonState() {}
-}
